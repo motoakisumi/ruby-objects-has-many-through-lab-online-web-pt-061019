b8f5d567b598c42b4fb5d8df4fc5dfcb50a020d2
Motoaki Sumi <msumi@mba2019.hbs.edu>
2019-08-14 07:22:39 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -17,7 +17,7 @@ class Patient
   end
 
   def appointments
-    Appointment.all.select{|patient| patient.appointments == self}
+    Appointment.all.select{|patient| appointment.patient == self}
   end
 
   def doctors
